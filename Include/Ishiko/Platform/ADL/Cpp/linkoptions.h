e2ff8ab9a74010cefa41aa7eb627b24ca4af26cc
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-04-25 14:20:31 +0100

    Update makefiles

---
+++ Include/Ishiko/Platform/ADL/Cpp/linkoptions.h
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-Platform/wxWidgetsEngine/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_PLATFORM_ADL_CPP_LINKOPTIONS_H_
+#define _ISHIKO_PLATFORM_ADL_CPP_LINKOPTIONS_H_
+
+#ifdef _DEBUG
+#pragma comment(lib, "IshikoADL-d.lib")
+#else
+#pragma comment(lib, "IshikoADL.lib")
+#endif
+
+#endif
