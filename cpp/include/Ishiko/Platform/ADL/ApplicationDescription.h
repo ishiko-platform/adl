ffad5ebea0595974ed109d29824e60d62591cbd9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-17 08:29:45 +0100

    Add files

--- cpp/include/Ishiko/Platform/ADL/ApplicationDescription.h
+++ cpp/include/Ishiko/Platform/ADL/ApplicationDescription.h
@@ -32,6 +32,4 @@ private:
 }
 }
 
-#include "linkoptions.h"
-
 #endif
---
+++ cpp/include/Ishiko/Platform/ADL/ApplicationDescriptionParser.h
@@ -0,0 +1,24 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-platform/adl/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_PLATFORM_ADL_CPP_APPLICATIONDESCRIPTIONPARSER_H_
+#define _ISHIKO_PLATFORM_ADL_CPP_APPLICATIONDESCRIPTIONPARSER_H_
+
+#include <string>
+
+namespace Ishiko
+{
+namespace Platform
+{
+
+class ApplicationDescriptionParser
+{
+};
+
+}
+}
+
+#endif
